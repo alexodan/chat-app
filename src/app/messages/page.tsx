4bdc6d00df1ef9a78fdaefd398c82885c0efd191
Alexis Diaz <alexdan1993@gmail.com>
2023-08-21 21:59:51 -0300

    Querying messages and adding MessagePreview component

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,30 +1,64 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import MessagePreview from '@/components/MessagePreview'
+import Button from '@/components/common/Button'
+import Link from 'next/link'
+import Image from 'next/image'
 import { redirect } from 'next/navigation'
 import { Database } from '@/types/supabase'
 import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { css } from '../../../styled-system/css'
 
 export default async function MessagesPage() {
   const supabase = createServerComponentClient<Database>({ cookies })
 
-  const { data } = await supabase.auth.getSession()
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
 
-  if (!data.session) {
+  if (!session) {
     return redirect('/login')
   }
 
-  const { data: contacts } = await supabase
-    .from('contacts')
-    .select('contact_id, user_id, contact_user_id')
-    .eq('user_id', data.session.user.id)
+  const { data: contacts } = await supabase.from('profiles').select('*')
+
+  // Note: I think there is no way to query Supabase for all chats that a user is in since users is an array type.
+  const { data: chats } = await supabase.from('chats').select('chat_id, users')
+  // So I have to filter manually.
+  const chatsForUser =
+    chats?.filter(chat => chat.users.includes(session.user.id)) || []
 
   return (
     <>
-      <h1>Messages</h1>
       <ul>
-        {contacts?.map(contact => (
-          <li key={contact.contact_id}>{contact.contact_user_id}</li>
-        ))}
+        {chatsForUser.map(chat => {
+          const userId = chat.users
+            .filter(userId => userId !== session.user.id)
+            .at(0)
+          // Note: For now I'm only handling one on one chats.
+          const contactInChat = contacts
+            ?.filter(contact => contact.id === userId)
+            .at(0)!
+
+          return (
+            <li key={chat.chat_id}>
+              <MessagePreview chatId={chat.chat_id} contact={contactInChat} />
+            </li>
+          )
+        })}
       </ul>
+      <Button
+        fullWidth={false}
+        className={css({ position: 'absolute', bottom: '4', right: '4' })}
+      >
+        <Link href="/messages/new">
+          <Image
+            src="/message.svg"
+            alt="New conversation"
+            height={30}
+            width={30}
+          />
+        </Link>
+      </Button>
     </>
   )
 }
